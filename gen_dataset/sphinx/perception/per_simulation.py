fce9941a634c16d96966bc0a29167fb7b4f5fb47
frederik <termite_pilgrim04@icloud.com>
2025-11-20 14:09:50 +0100

    4 gen

--- gen_dataset/sphinx/perception/per_simulation.py
+++ gen_dataset/sphinx/perception/per_simulation.py
@@ -4,14 +4,20 @@ import numpy as np
 
 from gen_dataset.dataset_schema import DatasetRow
 from gen_dataset.sphinx.perception.focus import (
+    can_you_loose,
+    can_you_win,
+    color_at_position,
     count_black_stones,
-    count_white_stones,
     count_empty_intersections,
-    color_at_position
+    count_white_stones,
+    where_are_the_black_stones,
+    where_are_the_white_stones,
 )
 
 
-def generate_perception_questions_for_episode(sim_id: int, simulated_game: np.ndarray) -> List[DatasetRow]:
+def generate_perception_questions_for_episode(
+    sim_id: int, simulated_game: np.ndarray
+) -> List[DatasetRow]:
     """
     Generates all the perception questions for a single simulated game
     """
@@ -30,10 +36,18 @@ def generate_perception_questions_for_episode(sim_id: int, simulated_game: np.nd
     rows.append(count_white_stones.gen_question_q8_sample(sim_id, simulated_game))
 
     # focus: count_empty_intersections
-    rows.append(count_empty_intersections.gen_question_q9_sample(sim_id, simulated_game))
-    rows.append(count_empty_intersections.gen_question_q10_sample(sim_id, simulated_game))
-    rows.append(count_empty_intersections.gen_question_q11_sample(sim_id, simulated_game))
-    rows.append(count_empty_intersections.gen_question_q12_sample(sim_id, simulated_game))
+    rows.append(
+        count_empty_intersections.gen_question_q9_sample(sim_id, simulated_game)
+    )
+    rows.append(
+        count_empty_intersections.gen_question_q10_sample(sim_id, simulated_game)
+    )
+    rows.append(
+        count_empty_intersections.gen_question_q11_sample(sim_id, simulated_game)
+    )
+    rows.append(
+        count_empty_intersections.gen_question_q12_sample(sim_id, simulated_game)
+    )
 
     # focus: color_at_position
     rows.append(color_at_position.gen_question_q13_sample(sim_id, simulated_game))
@@ -41,4 +55,43 @@ def generate_perception_questions_for_episode(sim_id: int, simulated_game: np.nd
     rows.append(color_at_position.gen_question_q15_sample(sim_id, simulated_game))
     rows.append(color_at_position.gen_question_q16_sample(sim_id, simulated_game))
 
+    # focus: can_you_win
+    rows.append(can_you_win.gen_question_q17_sample(sim_id, simulated_game))
+    rows.append(can_you_win.gen_question_q18_sample(sim_id, simulated_game))
+    rows.append(can_you_win.gen_question_q19_sample(sim_id, simulated_game))
+    rows.append(can_you_win.gen_question_q20_sample(sim_id, simulated_game))
+
+    # focus: can_you_loose
+    rows.append(can_you_loose.gen_question_q21_sample(sim_id, simulated_game))
+    rows.append(can_you_loose.gen_question_q22_sample(sim_id, simulated_game))
+    rows.append(can_you_loose.gen_question_q23_sample(sim_id, simulated_game))
+    rows.append(can_you_loose.gen_question_q24_sample(sim_id, simulated_game))
+
+    # focus: where_are_the_white_stones
+    rows.append(
+        where_are_the_white_stones.gen_question_q25_sample(sim_id, simulated_game)
+    )
+    rows.append(
+        where_are_the_white_stones.gen_question_q26_sample(sim_id, simulated_game)
+    )
+    rows.append(
+        where_are_the_white_stones.gen_question_q27_sample(sim_id, simulated_game)
+    )
+    rows.append(
+        where_are_the_white_stones.gen_question_q28_sample(sim_id, simulated_game)
+    )
+
+    # focus: where_are_the_black_stones
+    rows.append(
+        where_are_the_black_stones.gen_question_q29_sample(sim_id, simulated_game)
+    )
+    rows.append(
+        where_are_the_black_stones.gen_question_q30_sample(sim_id, simulated_game)
+    )
+    rows.append(
+        where_are_the_black_stones.gen_question_q31_sample(sim_id, simulated_game)
+    )
+    rows.append(
+        where_are_the_black_stones.gen_question_q32_sample(sim_id, simulated_game)
+    )
     return rows
