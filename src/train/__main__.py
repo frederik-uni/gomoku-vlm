24f83d7b75295f5e4ff133f3c8a8f2458612baf7
frederik <termite_pilgrim04@icloud.com>
2026-01-26 14:05:47 +0100

    .

--- src/train/__main__.py
+++ src/train/__main__.py
@@ -27,8 +27,9 @@ if __name__ == "__main__":
     #     print("No previous adapters found. Starting from base model.")
 
     if args.peft:
-        model.load_adapter(args.peft, adapter_name="vision_adapter", is_trainable=False)
-        model.set_adapter("vision_adapter")
+        model = PeftModel.from_pretrained(model, args.peft).eval()
+        # model.load_adapter(, adapter_name="vision_adapter", is_trainable=False)
+        # model.set_adapter("vision_adapter")
 
     new_output_dir = init_save(args.output_dir)
     print(f"New adapter will be saved to: {new_output_dir}")
